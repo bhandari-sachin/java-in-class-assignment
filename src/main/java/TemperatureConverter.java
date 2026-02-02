1088c873305856202a31759123603e4ca96dbf80
sachin bhandari <bhandari.sachin.fi@gmail.com>
2026-02-02 22:42:04 +0200

    Add TemperatureConverter.java

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,2 +1,13 @@
 public class TemperatureConverter {
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius - 32) * 5 / 9;
+    }
+
+    public boolean isExtremeTemperature(double celsius) {
+        return celsius < -40 || celsius > 50;
+    }
 }
